5cf720d95dbc599063d2ff7f04a377bfa152ec3c
Ilham <Ilham>
2022-07-25 20:56:03 +0300

    замена функционального компонента в классовый

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import TreeChart from "./components/TreeChart";
 import "./App.css"
 
 
-const App = () => {
-    const [data, setData] = useState([])
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        };
+    }
+    async componentDidMount() {
+        const response = await fetch(
+            `http://localhost:3001/data`
 
-    useEffect(() => {
-        fetch("http://localhost:3001/data")
-            .then(r => r.json())
-            .then(res => setData(res))
-    }, [])
-    console.log(data)
+        );
+        const newData = await response.json();
+        this.setState({ data: newData });
+    }
+    render() {
+const {data} = this.state;
+if (data){
+    return <React.Fragment>
+        <TreeChart data={data}/>
+    </React.Fragment>
+} else {return null}
+    }
 
-
-    return (
-        <React.Fragment>
-            <TreeChart data={data}/>
-
-        </React.Fragment>
-    );
-};
-
-export default App;
+}
