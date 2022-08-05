111b5311f8ef19cbf1dd61dcc41c094c689634cc
ISalikhzyanov <eazy591998@gmail.com>
2022-08-05 14:46:49 +0300

    allert fixed

--- src/components/TreeChart.jsx
+++ src/components/TreeChart.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, {useRef, useEffect} from "react";
 import {select, hierarchy, tree, linkHorizontal} from "d3";
 import useResizeObserver from "./useResizeObserver";
 
